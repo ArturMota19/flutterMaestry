cc99768d26462dcf917d907157c4bc411e4945ba
Artur <arturrodriguesmota@hotmail.com>
2024-04-30 10:45:53 -0300

    2: Initial setup to a quiz

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
 main (){
-  runApp(QuizApp());
+  runApp(const QuizApp());
 }
 
+final List<String> questions = [
+  "Who wanted to steal the philosopher's stone??",
+  "What is Harry Potter's father's name?",
+  "Who is Harry Potter's arch enemy (at Hogwarts)",
+];
+
 class QuizApp extends StatelessWidget{
+  const QuizApp({super.key});
+
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Quiz App'),
+          title: const Text('Quiz App'),
         ),
-        body: Text('Olá, mundo mobile! xD'),
-       ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('Voldemort'),
+            ),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('Quirrell'),
+            ),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('Quirrell and Voldemort'),
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
+
