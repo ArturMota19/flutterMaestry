04967656ecc27b30d0eb926b72892c512ddf148b
Artur <arturrodriguesmota@hotmail.com>
2024-05-02 10:03:41 -0300

    12: Add new form to add a ride

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -21,6 +21,15 @@ class RideExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final passangerController0 = TextEditingController();
+  final amountController0 = TextEditingController();
+  final passangerController1 = TextEditingController();
+  final amountController1 = TextEditingController();
+  final passangerController2 = TextEditingController();
+  final amountController2 = TextEditingController();
+  final passangerController3 = TextEditingController();
+  final amountController3 = TextEditingController();
+
   MyHomePage({super.key});
 
   final _rides = [
@@ -44,9 +53,7 @@ double totalAmount(Rides ride) {
         title: const Text('Ride Expenses'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
+        children: <Widget>[
           const SizedBox(
             width: double.infinity,
             child: Card(
@@ -105,9 +112,75 @@ double totalAmount(Rides ride) {
                 ],
               )
             )).toList(),
+          ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: passangerController0,
+                    decoration: InputDecoration(
+                      labelText: 'Passanger',
+                      ),
+                  ),
+                  TextField(
+                    controller: amountController0,
+                    decoration: InputDecoration(
+                      labelText: 'Amount (R\$)',
+                      ),
+                  ),
+                  TextField(
+                    controller: passangerController1,
+                    decoration: InputDecoration(
+                      labelText: 'Passanger',
+                      ),
+                  ),
+                  TextField(
+                    controller: amountController1,
+                    decoration: InputDecoration(
+                      labelText: 'Amount (R\$)',
+                      ),
+                  ),
+                  TextField(
+                    controller: passangerController2,
+                    decoration: InputDecoration(
+                      labelText: 'Passanger',
+                      ),
+                  ),
+                  TextField(
+                    controller: amountController2,
+                    decoration: InputDecoration(
+                      labelText: 'Amount (R\$)',
+                      ),
+                  ),
+                  TextField(
+                    controller: passangerController3,
+                    decoration: InputDecoration(
+                      labelText: 'Passanger',
+                      ),
+                  ),
+                  TextField(
+                    controller: amountController3,
+                    decoration: InputDecoration(
+                      labelText: 'Amount (R\$)',
+                      ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.purple),
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                    child: Text('Add Ride'),
+                  )
+                ],
+              ),
+            ),
           )
         ],
-      )
+      ),
     );
   }
 }
